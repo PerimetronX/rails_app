b60849b631eccaa7acefe6e500400bc0f34cb5d6
hiroto <occhi.1121@icloud.com>
2022-02-06 13:45:03 +0900

    GoogleMap機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "hello#index"
+  root "posts#index"
   get "search" => "posts#search"
   resources :posts do
     resources :likes, only: [:create, :destroy]
