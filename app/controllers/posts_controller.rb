74c50d543e5abe597cdaf346a9a0f9c5ac71d18e
hiroto <hironnium.2992@gmail.com>
2022-04-14 20:30:38 +0900

    ボタン

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,7 +47,7 @@ class PostsController < ApplicationController
 
     private
     def post_params
-        params.require(:post).permit(:title, :contents, :image, :address, :latitude, :longitude, :type, tag_ids: [])
+        params[:post].permit(:title, :contents, :image, :address, :latitude, :longitude, :type, tag_ids: [])
     end
 
 end
